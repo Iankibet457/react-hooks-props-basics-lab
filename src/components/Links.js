4091924fa8f98f996773934d6782ae973d8d1274
Ian <ian.mosonik@student.moringaschool.com>
2024-10-30 16:44:08 +0300

    completed lab

---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+import user from "../data/user";
+
+function Links(props){
+    return (
+        <div>
+
+            <h3>Links</h3>
+             <a href={props.github} target="_blank" rel="noopener noreferrer">{props.github}</a>
+      
+            <a href={props.linkedin} target="_blank" rel="noopener noreferrer">{props.linkedin}</a> 
+        </div>
+    )
+}
+
+export default Links;
